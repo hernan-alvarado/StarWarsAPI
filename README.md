b86b78b49c05c2e2f6736e6efebb0c9031b25302
Hernán Alvarado <hernnalvarado@gmail.com>
2023-09-01 17:40:57 -0400

    terminado

---
+++ README.md
@@ -0,0 +1,8 @@
+# Práctica Módulo 04
+
+- Consumo de API de Star Wars (https://swapi.dev/).
+- Se utiliza método Fetch.
+- Se muestran dimensiones de personajes de la saga (nombre, estatura y peso).
+
+Para la línea de tiempo se ocupa snippet de esta página:
+https://www.bootdey.com/snippets/view/timeline-events-area/
---
+++ assets/js/index.js
@@ -0,0 +1,149 @@
+function createCardElement(data, containerId) {
+	for (let i = 0; i < data.length; i++) {
+		const person = data[i];
+		//  datos del arreglo = person
+
+// Creo los elementos del DOM
+
+		let div = document.createElement("div");
+
+		let card = document.createElement("p");
+		card.className = "col col-md-6 col-lg-4";
+		
+
+		let cardBody = document.createElement("div");
+		cardBody.className = "single-timeline-content d-flex wow fadeInLeft data-wow-delay='0.3s'";
+		cardBody.style = "background-color: white";
+
+
+		let circleDiv = document.createElement("p");
+		circleDiv.style.height = "35px";
+		circleDiv.style.width = "35px";
+	
+		let image = document.createElement("img");
+		image.src =
+			"https://i.pinimg.com/1200x/a2/a2/27/a2a227afa5d96e329085b989357b1129.jpg";
+		image.alt = "Logo Star Wars iluminado";
+		image.style.height = "50px"; 
+		image.style.width = "50px"; 
+
+		let textDiv = document.createElement("div");
+		textDiv.className = "time-line mx-4";
+
+		let nameElement = document.createElement("h5");
+		nameElement.innerText = person.name;
+
+		let subtitleElement = document.createElement("h8");
+		subtitleElement.className = "card-subtitle mb-1";
+		subtitleElement.innerHTML = `<span><b>Estatura:</b> ${person.height} <b>Peso:</b> ${person.mass}</span>`;
+
+		circleDiv.appendChild(image);
+		textDiv.appendChild(nameElement);
+		textDiv.appendChild(subtitleElement);
+		cardBody.appendChild(circleDiv);
+		cardBody.appendChild(textDiv);
+		card.appendChild(cardBody);
+		div.appendChild(card);
+
+		document.getElementById(containerId).appendChild(card);
+	}
+}
+
+// CONSUMO DE API.
+
+// Recorrido hasta la posicion 5.
+
+
+function fetchprimerGrupo() {
+	setTimeout(3000)
+	const promises = [];
+	for (let i = 1; i <= 5; i++) {
+		const promise = fetch(`https://swapi.dev/api/people/${i}/`)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("Data obtenida:", data);
+
+				document.getElementById("informacioncantidadUno").innerHTML = `1 - ${
+					Object.keys(data).length - 11
+				}`;
+
+				return data;
+			})
+			.catch((error) => console.error("Error:", error));
+
+		promises.push(promise);
+	}
+
+	return Promise.all(promises);
+}
+
+// Recorrido de la posicion 6 a la 11.
+
+function fetchsegundoGrupo() {
+	setTimeout(3000);
+	const promises = [];
+	for (let i = 6; i <= 11; i++) {
+		const promise = fetch(`https://swapi.dev/api/people/${i}/`)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("Data obtenida:", data);
+				document.getElementById("informacioncantidadDos").innerText = `6 - ${
+					Object.keys(data).length - 5
+				}`;
+
+				return data;
+			})
+			.catch((error) => console.error("Error:", error));
+
+		promises.push(promise);
+	}
+
+	return Promise.all(promises);
+}
+
+// Recorrido de la posicion 12 a la 16.
+
+function fetchtercerGrupo() {
+	setTimeout(3000);
+	const promises = [];
+	for (let i = 12; i <= 16; i++) {
+		const promise = fetch(`https://swapi.dev/api/people/${i}/`)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("Data obtenida:", data);
+				document.getElementById("informacioncantidadTres").innerText = `12 - ${
+					Object.keys(data).length 
+				}`;
+
+				return data;
+			})
+			.catch((error) => console.error("Error:", error));
+
+		promises.push(promise);
+	}
+
+	return Promise.all(promises);
+}
+
+
+
+$("#despliegaCartasUno").one("mousemove", function () {
+	console.log("Entramos en el evento uno");
+	fetchprimerGrupo()
+		.then((data) => createCardElement(data, "informacioncardsUno"))
+		.catch((error) => console.error("Error:", error));
+});
+
+$("#despliegaCartasDos").one("mousemove", function () {
+	console.log("Entramos en el evento dos");
+	fetchsegundoGrupo()
+		.then((data) => createCardElement(data, "informacioncardsDos"))
+		.catch((error) => console.error("Error:", error));
+});
+
+$("#despliegaCartasTres").one("mousemove", function () {
+	console.log("Entramos en el evento tres");
+	fetchtercerGrupo()
+		.then((data) => createCardElement(data, "informacioncardsTres"))
+		.catch((error) => console.error("Error:", error));
+});
